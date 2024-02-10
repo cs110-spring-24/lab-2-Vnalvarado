4c6fcedbe84d791d105dd5af405dbe31bebb1de8
Vnalvarado <157657964+Vnalvarado@users.noreply.github.com>
2024-02-09 23:50:33 -0800

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,9 +4,18 @@ num = random.randint(1, 100)
 user = input("Enter your guess: ")
 user = int(user)
 
+Count=0
 if user > num:
     print("Too high, it was", num)
+    Count+=1
+
+Count=1
 elif user < num:
     print("Too low, it was", num)
+    count+=2
+
+Count=2
 else:
     print("You got it!")
+    count+=3
+
